5363d31ebec4b5c053c5a3b4e832515949c34e26
Luke Bennett <luke.bennett88@gmail.com>
2019-05-07 21:00:44 +1000

    Update Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,15 +4,11 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 import '../../css/tailwind.css';
-import '../../css/typography.css';
-import '../../css/global.css';
 
 const Layout = ({ children }) => (
-  <div className="body antialiased flex flex-col leading-relaxed min-h-screen overflow-hidden text-gray-700 w-full">
+  <div className="antialiased flex flex-col leading-relaxed min-h-screen text-gray-700 w-full">
     <Header />
-    <main className="bg-white flex-1 max-w-1920 mx-auto w-full">
-      {children}
-    </main>
+    <main className="bg-white flex-1 mx-auto w-full">{children}</main>
     <Footer />
   </div>
 );
