ea6e2fba0a6d6e1dc5e53769fb1f69d4f1c9f25e
Luke Bennett <hello@lukebennett.com.au>
2019-06-16 16:09:07 +1000

    Style success and 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
@@ -6,8 +7,20 @@ import SEO from '../components/SEO';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-1 items-center justify-center mx-auto w-full">
+      <div className="max-w-sm p-4">
+        <h1 className="leading-none mb-2 pt-2 text-4xl">404: Page Not Found</h1>
+        <p>The page you were looking for doesn't seem to exist.</p>
+        <p className="mt-4">
+          <Link
+            className="bg-blue-700 hover:bg-blue-600 font-semibold inline-block leading-none px-4 py-2 text-white text-xs tracking-wide uppercase"
+            to="/"
+          >
+            Back to homepage
+          </Link>
+        </p>
+      </div>
+    </div>
   </Layout>
 );
 
--- src/pages/success.js
+++ src/pages/success.js
@@ -6,20 +6,20 @@ import SEO from '../components/SEO';
 const SuccessPage = () => (
   <Layout>
     <SEO title="Success: message sent" />
-    <div className="flex items-center justify-center mx-auto w-full">
+    <div className="flex flex-1 items-center justify-center mx-auto w-full">
       <div className="max-w-sm p-4">
-        <h2 className="leading-none m-0 pt-2 text-4xl">
+        <h1 className="leading-none mb-2 pt-2 text-4xl">
           Form submitted successfully!
-        </h2>
+        </h1>
         <p>
           Thank you for your message. We will get back to you as soon as we can.
         </p>
-        <p>
+        <p className="mt-4">
           <Link
-            className="bg-blue-700 hover:bg-blue-600 cursor-pointer font-semibold leading-none px-4 py-2 rounded-full text-white text-xs tracking-wide uppercase"
+            className="bg-blue-700 hover:bg-blue-600 font-semibold inline-block leading-none px-4 py-2 text-white text-xs tracking-wide uppercase"
             to="/"
           >
-            Return to home
+            Back to homepage
           </Link>
         </p>
       </div>
